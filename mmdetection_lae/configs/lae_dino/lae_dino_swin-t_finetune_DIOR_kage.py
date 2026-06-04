dbeef5542d9a7d292d5dbdc198493142885b08d7
박혜림 <parkhyellim@baghyelim-ui-MacBookAir.local>
2026-06-04 19:14:53 +0900

    Fix KAGE descriptor path

--- mmdetection_lae/configs/lae_dino/lae_dino_swin-t_finetune_DIOR_kage.py
+++ mmdetection_lae/configs/lae_dino/lae_dino_swin-t_finetune_DIOR_kage.py
@@ -2,7 +2,7 @@ _base_ = './lae_dino_swin-t_finetune_DIOR.py'
 
 model = dict(
     kage_cfg=dict(
-        descriptor_path='tools/kage/dior_descriptors.json',
+        descriptor_path='../tools/kage/dior_descriptors.json',
         roi_output_size=7,
         expand_scale=1.5,
         topk=3,
